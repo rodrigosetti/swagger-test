13542c8adb629c9e248bd5ec4bcb027397c40274
Rodrigo Setti <rodrigosetti@gmail.com>
2017-08-24 19:36:39 -0700

    some progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
-module Main where
+module Main (main) where
 
-import           Control.Monad              (forM_)
-import           Data.Aeson                 (encode)
-import qualified Data.ByteString            as BS
-import qualified Data.ByteString.Char8      as C
-import qualified Data.ByteString.Lazy.Char8 as LC
-import           Data.CaseInsensitive       (original)
-import           Data.Maybe                 (fromMaybe)
-import           System.Exit                (die)
+import           Control.Monad         (forM_)
+import qualified Data.ByteString       as BS
+import qualified Data.ByteString.Char8 as C
+import           Data.CaseInsensitive  (original)
+import           Data.Maybe            (fromMaybe)
+import           System.Exit           (die)
 import           Test.Swagger.Gen
 
 main :: IO ()
@@ -25,5 +23,5 @@ printRequest (JsonHTTPRequest host method path headers body) =
      putStrLn path
      forM_ headers $ \(k,v) -> BS.putStr (original k) >> putStr ": " >> C.putStrLn v
      case body of
-       Just b  -> putStr "\n" >> LC.putStrLn (encode b)
+       Just b  -> putStr "\n" >> C.putStrLn b
        Nothing -> pure ()
--- src/Test/Swagger/Gen.hs
+++ src/Test/Swagger/Gen.hs
@@ -8,6 +8,7 @@ import           Data.Aeson
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as C
 import qualified Data.HashMap.Strict.InsOrd as M
+import           Data.List                  (partition)
 import           Data.Maybe
 import           Data.Swagger
 import           Network.HTTP.Types
@@ -21,7 +22,7 @@ data JsonHTTPRequest = JsonHTTPRequest { requestHost    :: Maybe FullyQualifiedH
                                        , requestMethod  :: Method
                                        , requestPath    :: String
                                        , requestHeaders :: RequestHeaders
-                                       , requestBody    :: Maybe Value }
+                                       , requestBody    :: Maybe BS.ByteString }
                                   deriving (Show, Eq)
 
 -- |Given a swagger.json (encoded as a bytestring), decode it, and produce a
@@ -37,12 +38,40 @@ generateRequest = generate . requestGenerator
 
 -- Random Request generator
 requestGenerator :: Swagger -> Gen JsonHTTPRequest
-requestGenerator s = do (path, item) <- elements $ M.toList $ s ^. paths
-                        let baseP = fromMaybe "/" $ s ^. basePath
+requestGenerator s = do let baseP = fromMaybe "/" $ s ^. basePath
                         let mHost = s ^. host
+                        -- pick a path
+                        (path, item) <- elements $ M.toList $ s ^. paths
+                        -- select one operation of the selected path
+                        (method, operation) <- elements $ catMaybes [ (methodGet,) <$> item ^. get
+                                                                    , (methodPut,) <$> item ^. put
+                                                                    , (methodPost,) <$> item ^. post
+                                                                    , (methodDelete,) <$> item ^. delete
+                                                                    , (methodOptions,) <$> item ^. options
+                                                                    , (methodHead,) <$> item ^. head_
+                                                                    , (methodPatch,) <$> item ^. patch ]
+
+                        let resolveRefParam :: Referenced Param -> Maybe Param
+                            resolveRefParam (Inline p) = Just p
+                            resolveRefParam (Ref r)    = M.lookup (getReference r) $ s ^. parameters
+
+                        -- combine parameters common to all operations to parameters
+                        -- specific to the selected operation
+                        let params = catMaybes $ resolveRefParam <$> (item ^. parameters) ++ (operation ^. parameters)
+
+                        -- partition between required and non-required parameters
+                        let (requiredParams, notRequiredParams) = partition paramIsRequired params
+
+                        selectedOptionalParams <- sublistOf notRequiredParams
+
+                        let finalParams = requiredParams ++ selectedOptionalParams
+
+                        -- apply finalParams
+                        -- TODO
+
                         scheme <- elements $ fromMaybe [Https] $ s ^. schemes
                         pure $ JsonHTTPRequest (buildHost scheme <$> mHost)
-                                                "POST"
+                                                method
                                                 (joinPath [baseP, path])
                                                 (catMaybes [(("Host",) . C.pack . (^. name)) <$> mHost])
                                                 Nothing
@@ -55,3 +84,7 @@ requestGenerator s = do (path, item) <- elements $ M.toList $ s ^. paths
       schemeToHttpPrefix Https = "https://"
       schemeToHttpPrefix Ws    = "ws://"
       schemeToHttpPrefix Wss   = "wss://"
+
+paramIsRequired :: Param -> Bool
+paramIsRequired Param { _paramSchema = ParamOther (ParamOtherSchema { _paramOtherSchemaIn = ParamPath})} = True
+paramIsRequired p = fromMaybe False $ p ^. required
