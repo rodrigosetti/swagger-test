c92bbc9359a4b8fda41674601fbb03ffab0e0c14
Rodrigo Setti <rodrigosetti@gmail.com>
2017-08-25 17:43:36 -0700

    more progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main (main) where
 
-import           Control.Monad         (forM_)
-import qualified Data.ByteString       as BS
-import qualified Data.ByteString.Char8 as C
-import           Data.CaseInsensitive  (original)
-import           Data.Maybe            (fromMaybe)
-import           System.Exit           (die)
+import           Control.Monad              (forM_)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Char8      as C
+import qualified Data.ByteString.Lazy.Char8 as LC
+import           Data.CaseInsensitive       (original)
+import           Data.Maybe                 (fromMaybe)
+import           System.Exit                (die)
 import           Test.Swagger.Gen
 
 main :: IO ()
@@ -17,8 +18,8 @@ printRequest (JsonHTTPRequest host method path headers body) =
   do BS.putStr method
      putStr " "
      putStr $ fromMaybe "" host
-     putStrLn path
+     BS.putStrLn path
      forM_ headers $ \(k,v) -> BS.putStr (original k) >> putStr ": " >> C.putStrLn v
      case body of
-       Just b  -> putStr "\n" >> C.putStrLn b
+       Just b  -> putStr "\n" >> LC.putStrLn b
        Nothing -> pure ()
--- src/Test/Swagger/Gen.hs
+++ src/Test/Swagger/Gen.hs
@@ -1,44 +1,56 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections     #-}
-module Test.Swagger.Gen ( JsonHTTPRequest(..)
+module Test.Swagger.Gen ( HTTPRequest(..)
                         , generateRequestFromJsonDefinition) where
 
+import           Control.Applicative        ((<|>))
+import           Control.Arrow              ((&&&))
 import           Control.Lens               hiding (elements)
 import           Data.Aeson
+import           Data.Binary.Builder
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as C
+import qualified Data.ByteString.Lazy       as LBS
+import           Data.CaseInsensitive       (mk)
 import qualified Data.HashMap.Strict.InsOrd as M
 import           Data.List                  (partition)
 import           Data.Maybe
 import           Data.Monoid                ((<>))
 import           Data.Swagger
+import           Data.Swagger.Internal      (SwaggerKind (..))
+import qualified Data.Text                  as T
+import           Data.Text.Encoding         (encodeUtf8)
 import           Network.HTTP.Types
+import           Network.HTTP.Types.Header
+import           Network.HTTP.Types.URI
 import           System.FilePath.Posix      (joinPath)
 import           Test.QuickCheck
 
 -- |The FullyQualifiedHost contains the scheme (i.e. http://), hostname and port.
 type FullyQualifiedHost = String
 
-data JsonHTTPRequest = JsonHTTPRequest { requestHost    :: Maybe FullyQualifiedHost
-                                       , requestMethod  :: Method
-                                       , requestPath    :: String
-                                       , requestHeaders :: RequestHeaders
-                                       , requestBody    :: Maybe BS.ByteString }
+data HTTPRequest = HTTPRequest { requestOperationId :: Maybe String
+                               , requestHost        :: Maybe FullyQualifiedHost
+                               , requestMethod      :: Method
+                               , requestPath        :: BS.ByteString
+                               , requestHeaders     :: RequestHeaders
+                               , requestBody        :: Maybe LBS.ByteString }
                                   deriving (Show, Eq)
 
 -- |Given a swagger.json (encoded as a bytestring), decode it, and produce a
 --  random Request that complies with the schema.
 --  The return type is Either a parsing error (described as String), or a
 --  random Request (in the IO monad because it's random).
-generateRequestFromJsonDefinition :: BS.ByteString -> Either String (IO JsonHTTPRequest)
+generateRequestFromJsonDefinition :: BS.ByteString -> Either String (IO HTTPRequest)
 generateRequestFromJsonDefinition = fmap generateRequest . eitherDecodeStrict
 
 -- Generate a random request for a Swagger definition
-generateRequest :: Swagger -> IO JsonHTTPRequest
+generateRequest :: Swagger -> IO HTTPRequest
 generateRequest = generate . requestGenerator
 
 -- Random Request generator
-requestGenerator :: Swagger -> Gen JsonHTTPRequest
+requestGenerator :: Swagger -> Gen HTTPRequest
 requestGenerator s = do let baseP = fromMaybe "/" $ s ^. basePath
                             mHost = s ^. host
                         -- pick a path
@@ -56,6 +68,10 @@ requestGenerator s = do let baseP = fromMaybe "/" $ s ^. basePath
                             resolveRefParam (Inline p) = Just p
                             resolveRefParam (Ref r)    = M.lookup (getReference r) $ s ^. parameters
 
+                        let resolveRefSchema :: Referenced Schema -> Maybe Schema
+                            resolveRefSchema (Inline p) = Just p
+                            resolveRefSchema (Ref r)    = M.lookup (getReference r) $ s ^. definitions
+
                         -- combine parameters common to all operations to parameters
                         -- specific to the selected operation
                         let params = catMaybes $ resolveRefParam <$> (item ^. parameters) <> (operation ^. parameters)
@@ -64,17 +80,52 @@ requestGenerator s = do let baseP = fromMaybe "/" $ s ^. basePath
 
                         selectedOptionalParams <- sublistOf notRequiredParams
 
+                        -- final list of parameters that must be applied
                         let finalParams = requiredParams <> selectedOptionalParams
 
-                        -- apply finalParams
-                        -- TODO
+                        -- pick params for path
+                        let pathParams = catMaybes (paramSchemaAndAllowEmpty ParamPath <$> finalParams)
+                        path' <- applyPathTemplating pathParams $ T.pack path
 
-                        scheme <- elements $ fromMaybe [Https] $ s ^. schemes
-                        pure $ JsonHTTPRequest (buildHost scheme <$> mHost)
-                                                method
-                                                (joinPath [baseP, path])
-                                                (catMaybes [(("Host",) . C.pack . (^. name)) <$> mHost])
-                                                Nothing
+                        -- pick params for query string
+                        let queryParams = catMaybes $ paramSchemaAndAllowEmpty ParamQuery <$> finalParams
+
+                        queryStr <- genQuery queryParams
+
+                        let pathAndQuery = C.pack (joinPath [baseP, T.unpack path'])
+                                         <> renderQuery True queryStr
+
+                        -- pick params for header
+                        let headerParams = catMaybes (paramSchemaAndAllowEmpty ParamHeader <$> finalParams)
+
+                        headers <- genQuery headerParams
+
+                        -- pick params for form data
+                        let formDataParams = catMaybes $ paramSchemaAndAllowEmpty ParamFormData <$> finalParams
+
+                        maybeMimeAndBody <- if null formDataParams
+                                             then do -- pick a param for body
+                                                     bodySchema <- maybeElements $ catMaybes $ (resolveRefSchema =<<) . bodySchemaParam <$> finalParams
+                                                     randomJsonBody <- maybe (pure Nothing) (Just <$>) $ genJSON <$> bodySchema
+                                                     pure $ (("application/json",) . encode) <$> randomJsonBody
+                                             else do formDataQuery <- genQuery formDataParams
+                                                     pure $ Just ( "application/x-www-form-urlencoded"
+                                                                 , toLazyByteString $ renderQueryBuilder False formDataQuery)
+
+                        let headers' = catMaybes
+                                     $   (\h -> (fst h,) <$> snd h)
+                                     <$> ((mk . fst &&& snd) <$> headers)
+                                     <>  [(hHost, (C.pack . (^. name)) <$> mHost)]
+                                     <>  [(hContentType, fst <$> maybeMimeAndBody)]
+
+                        -- use scheme from operation, if defined, or from global
+                        scheme <- elements $ fromMaybe [Https] (operation ^. schemes <|> s ^. schemes)
+                        pure $ HTTPRequest (T.unpack <$> operation ^. operationId)
+                                           (buildHost scheme <$> mHost)
+                                           method
+                                           pathAndQuery
+                                           headers'
+                                           (snd <$> maybeMimeAndBody)
 
     where
       buildHost :: Scheme -> Host -> String
@@ -85,6 +136,44 @@ requestGenerator s = do let baseP = fromMaybe "/" $ s ^. basePath
       schemeToHttpPrefix Ws    = "ws://"
       schemeToHttpPrefix Wss   = "wss://"
 
+      bodySchemaParam :: Param -> Maybe (Referenced Schema)
+      bodySchemaParam Param { _paramSchema = ParamBody r} = Just r
+      bodySchemaParam _                                   = Nothing
+
+      applyPathTemplating :: [(T.Text, ParamSchema SwaggerKindParamOtherSchema, Bool)] -> T.Text -> Gen T.Text
+      applyPathTemplating [] p                 = pure p
+      applyPathTemplating ((name, sc, ae):ts) p =
+        do v <- paramGen sc ae
+           applyPathTemplating ts $ T.replace p ("{" <> name <> "}") v
+
+      genQuery :: [(T.Text, ParamSchema SwaggerKindParamOtherSchema, Bool)] -> Gen Query
+      genQuery []                  = pure []
+      genQuery ((name, sc, ae):ts) =
+        do v <- (\p -> if T.null p then Nothing else Just p) <$> paramGen sc ae
+           ((encodeUtf8 name, encodeUtf8 <$> v):) <$> genQuery ts
+
+      paramSchemaAndAllowEmpty :: ParamLocation -> Param -> Maybe (T.Text, ParamSchema SwaggerKindParamOtherSchema, Bool)
+      paramSchemaAndAllowEmpty loc Param { _paramName = n, _paramSchema = ParamOther pos@ParamOtherSchema {} }
+          | loc == pos ^. in_ = Just ( n
+                                     , pos ^. paramSchema
+                                     , (loc == ParamQuery || loc == ParamFormData) && fromMaybe False (pos ^. allowEmptyValue))
+          | otherwise = Nothing
+
+-- |Useful combinator for (Gen a) family: chose one of the values or
+-- Nothing if the list is empty. (i.e. safe "elements")
+maybeElements :: [a] -> Gen (Maybe a)
+maybeElements [] = pure Nothing
+maybeElements xs = (Just . (xs !!)) <$> choose (0, length xs - 1)
+
 paramIsRequired :: Param -> Bool
 paramIsRequired Param { _paramSchema = ParamOther ParamOtherSchema { _paramOtherSchemaIn = ParamPath}} = True
 paramIsRequired p = fromMaybe False $ p ^. required
+
+-- |Generator for a parameter, which is used on the "path", "query", "form", or
+-- "header".
+paramGen :: ParamSchema SwaggerKindParamOtherSchema -> Bool -> Gen T.Text
+paramGen sc allowEmpty = _
+
+-- |Generate a JSON from a schema
+genJSON :: Schema -> Gen Value
+genJSON s = _
